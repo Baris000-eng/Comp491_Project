66eeec47c81044fcd8dded611f6117230e11b6af
lato99 <72751305+lato99@users.noreply.github.com>
2023-03-18 22:44:51 +0300

    report it problems dashboard

--- app.py
+++ app.py
@@ -1,19 +1,24 @@
+import sqlite3
+from flask import Flask, render_template, request, redirect, url_for
 from flask import Flask, request, render_template, url_for, redirect
 
 app = Flask(__name__)
 
-from flask import Flask, render_template, request, redirect, url_for
-import sqlite3
 
-app = Flask(__name__)
-    
 @app.route('/signup_success')
 def signup_success():
     return "Signup successful!"
 
+
 @app.route('/', methods=['GET', 'POST'])
 def opening_screen():
     return render_template("opening_screen.html")
+
+
+@app.route('/')
+def index():
+    return render_template('student_dashboard.html')
+
 
 @app.route('/role_selection', methods=['POST'])
 def role_selection():
@@ -33,18 +38,20 @@ def role_selection():
 def teacher_screen():
     return render_template('teacher_screen.html')
 
+
 @app.route('/student_screen')
 def student_screen():
     return render_template('student_screen.html')
+
 
 @app.route('/it_staff_screen')
 def it_staff_screen():
     return render_template('it_staff_screen.html')
 
+
 @app.route('/student_dashboard')
 def student_dashboard():
     return render_template('student_dashboard.html')
-
 
 
 @app.route('/student_signup', methods=['GET', 'POST'])
@@ -64,7 +71,8 @@ def student_signup():
                      password TEXT NOT NULL)''')
 
         # Check if the username-password pair already exists in the database
-        c.execute("SELECT * FROM students_signup_db WHERE username = ? AND password = ?", (username, password))
+        c.execute(
+            "SELECT * FROM students_signup_db WHERE username = ? AND password = ?", (username, password))
         existing_student = c.fetchone()
         if existing_student:
             # Display error message if student already exists
@@ -72,7 +80,8 @@ def student_signup():
             return render_template('student_signup.html', error_message=error_message)
         else:
             # Insert the new student into the database
-            c.execute("INSERT INTO students_signup_db (username, password) VALUES (?, ?)", (username, password))
+            c.execute(
+                "INSERT INTO students_signup_db (username, password) VALUES (?, ?)", (username, password))
             conn.commit()
             conn.close()
 
@@ -88,46 +97,91 @@ def student_signup():
 def student_login():
     if request.method == 'POST':
         # Handle form submission here
-        return redirect('/student_screen') # burdda student dashboardduna redirect etmesi gerekiyor (chat kısmı, sınıf bilgisi falan bburda olcak)
+        # burdda student dashboardduna redirect etmesi gerekiyor (chat kısmı, sınıf bilgisi falan bburda olcak)
+        return redirect('/student_screen')
     else:
         return render_template('student_login.html')
-    
+
+
 @app.route('/it_staff_signup', methods=['GET', 'POST'])
 def it_staff_signup():
     if request.method == 'POST':
         # Handle form submission here
-        return redirect('/it_staff_screen') # burdda it_staff dashboardduna redirect etmesi gerekiyor (chat kısmı, it panel, sınıf bilgisi falan bburda olcak)
+        # burdda it_staff dashboardduna redirect etmesi gerekiyor (chat kısmı, it panel, sınıf bilgisi falan bburda olcak)
+        return redirect('/it_staff_screen')
     else:
         return render_template('it_staff_signup.html')
-    
+
 
 @app.route('/it_staff_login', methods=['GET', 'POST'])
 def it_staff_login():
     if request.method == 'POST':
         # Handle form submission here
-        return redirect('/it_staff_screen') # burdda it_staff dashboardduna redirect etmesi gerekiyor (chat kısmı, it panel, sınıf bilgisi falan bburda olcak)
+        # burdda it_staff dashboardduna redirect etmesi gerekiyor (chat kısmı, it panel, sınıf bilgisi falan bburda olcak)
+        return redirect('/it_staff_screen')
     else:
         return render_template('it_staff_login.html')
-    
+
+
 @app.route('/teacher_signup', methods=['GET', 'POST'])
 def teacher_signup():
     if request.method == 'POST':
         # Handle form submission here
-        return redirect('/teacher_screen') # burdda teacher dashboardduna redirect etmesi gerekiyor (chat kısmı, it panel, sınıf bilgisi falan bburda olcak)
+        # burdda teacher dashboardduna redirect etmesi gerekiyor (chat kısmı, it panel, sınıf bilgisi falan bburda olcak)
+        return redirect('/teacher_screen')
     else:
         return render_template('teacher_signup.html')
-    
+
 
 @app.route('/teacher_login', methods=['GET', 'POST'])
 def teacher_login():
     if request.method == 'POST':
         # Handle form submission here
-        return redirect('/teacher_screen') # burdda teacher dashboardduna redirect etmesi gerekiyor (chat kısmı, it panel, sınıf bilgisi falan bburda olcak)
+        # burdda teacher dashboardduna redirect etmesi gerekiyor (chat kısmı, it panel, sınıf bilgisi falan bburda olcak)
+        return redirect('/teacher_screen')
     else:
         return render_template('teacher_login.html')
-    
-if __name__=='main':
-    app.run(debug=True, host="127.0.0.1", port="5000")
+
+
+@app.route('/reserve', methods=['POST'])
+def reserve():
+    class_code = request.form['class-code']
+    time = request.form['time']
+    date = request.form['date']
+    option = request.form['option']
+
+    with open('class_reservations.txt', 'a') as f:
+        f.write(f'Class Code: {class_code}\n')
+        f.write(f'Time: {time}\n')
+        f.write(f'Date: {date}\n')
+        f.write(f'Option: {option}\n\n')
+
+    return 'Reservation submitted successfully!'
+
+
+@app.route('/reportingIT', methods=['POST'])
+def report_it():
+    room_number = request.form['room_number']
+    faculty_name = request.form['faculty_name']
+    problem_description = request.form['problem_description']
+    date = request.form['date']
+    time = request.form['time']
+
+    # Write the form data to the file
+    with open('IT_Problems.txt', 'a') as f:
+        f.write(f'Room Number: {room_number}\n')
+        f.write(f'Faculty Name: {faculty_name}\n')
+        f.write(f'Problem Description: {problem_description}\n')
+        f.write(f'Date: {date}\n')
+        f.write(f'Time: {time}\n\n')
+
+    # Return a response to the user
+    return 'Thank you for reporting the problem to IT!'
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
 
 
 
