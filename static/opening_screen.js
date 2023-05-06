56934743e171b4996775649330b5a10b1d3ed92c
Baris000-eng <bkaplan18@ku.edu.tr>
2023-05-06 15:30:55 +0300

    Removed tiring css stylings and improved overall ui

--- static/opening_screen.js
+++ static/opening_screen.js
@@ -2,17 +2,21 @@ function changeBackgroundColor(color) {
   document.body.style.backgroundColor = color;
 }
 
-let imageIndex = 1;
-	let selectedImage = document.querySelector(".image");
-	let imagesList = ["../static/images/img1.png", "../static/images/img2.png", "../static/images/img3.png"];
-	setInterval(() => {
-		if (imageIndex + 1 > imagesList.length) {
-			imageIndex = 0;
-		}
+document.addEventListener("DOMContentLoaded", function() {
+  const images = [
+    "../static/images/img1.png",
+    "../static/images/img2.png",
+    "../static/images/img3.png"
+  ];
+  
+  let currentImageIndex = 0;
+  const imageElement = document.querySelector(".image");
+  
+  setInterval(() => {
+    currentImageIndex = (currentImageIndex + 1) % images.length;
+    const nextImageSrc = images[currentImageIndex];
+    imageElement.setAttribute("src", nextImageSrc);
+  }, 1000);
+});
 
-		selectedImage.src = imagesList[imageIndex];
-		imageIndex = imageIndex + 1;
-	}, 1000);
-
-	const form = document.querySelector('form');
 
