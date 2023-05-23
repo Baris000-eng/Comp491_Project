c09fd3e61445659ef6ff928ec9238252dbccf1a5
Baris000-eng <bkaplan18@ku.edu.tr>
2023-05-23 16:10:55 +0300

    added more mail-like structure to the sending reservation info and code functionality

--- service/MailSendingService.py
+++ service/MailSendingService.py
@@ -1,40 +1,48 @@
 import smtplib
-from flask import request
+from flask import request, render_template
 import service.ReservationService as RS
-from flask import render_template
 
 def sendReservationInformationAsEmail():
     reservation_info = RS.getReservationInformation()
     reservation_code = RS.generate_classroom_reservation_code()
-    print("Reservation Information: "+str(reservation_info))
+    print("Reservation Information: " + str(reservation_info))
+
     email = 'bkaplan10001@gmail.com'
     receiver_email = request.form.get('reservationInfoReceiver')
     random_password = 'cvvbsgxqkuziqgsd'
     protocol = 'smtp.gmail.com'
-    
-    email_subject = 'Your Reservation Information'
-    email_content = 'Reservation Information:\n\n'
-    email_content += f'Your reservation code: {reservation_code}\n'
-    email_content += f'Your username: {reservation_info[0]}\n'
-    email_content += f'Your role: {reservation_info[1]}\n'
-    email_content += f'Your priority: {reservation_info[2]}\n'
-    email_content += f'Code of Reserved Class: {reservation_info[3]}\n'
-    email_content += f'Reservation Start Time: {reservation_info[4]}\n'
-    email_content += f'Reservation End Time: {reservation_info[5]}\n'
 
-    reservation_duration = RS.calculateDuration(reservation_info[4], reservation_info[5])
-    email_content += f'Reservation Duration: {reservation_duration} minutes\n'
-    email_content += f'Reservation Date: {reservation_info[6]}\n'
-    email_content += f'Reservation Purpose: {reservation_info[7]}\n'
-    
-    message = f'Subject: {email_subject}\n\n{email_content}'
-    
+    email_subject = 'Your Reservation Information'
+    email_content = f"""Subject: {email_subject}
+
+    Dear {reservation_info[0]},
+
+    Thank you for making a reservation. Below are the details of your reservation:
+
+    Reservation Code: {reservation_code}
+    Reserver Username: {reservation_info[0]}
+    Reserver Role: {reservation_info[1]}
+    Reserver Priority: {reservation_info[2]}
+    Reserved Class Code: {reservation_info[3]}
+    Reservation Start Time: {reservation_info[4]}
+    Reservation End Time: {reservation_info[5]}
+    Reservation Duration: {RS.calculateDuration(reservation_info[4], reservation_info[5])} minutes
+    Reservation Date: {reservation_info[6]}
+    Reservation Purpose: {reservation_info[7]}
+
+    If you have any questions or need further assistance, please feel free to contact us.
+
+    Best regards,
+    KuClass Team
+    """
+
     smt = smtplib.SMTP(protocol, 587)
     smt.ehlo()
     smt.starttls()
     smt.login(email, random_password)
-    smt.sendmail(email, receiver_email, message)
+    smt.sendmail(email, receiver_email, email_content)
     smt.quit()
-    
+
     return render_template("successful_reservation_information_sending.html")
+
 
