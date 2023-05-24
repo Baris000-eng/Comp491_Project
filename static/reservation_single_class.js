9498e1f71eb4020f6151f126a257add1c78f1cba
Baris000-eng <bkaplan18@ku.edu.tr>
2023-05-24 16:05:51 +0300

    UI improvements

--- static/reservation_single_class.js
+++ static/reservation_single_class.js
@@ -1,7 +1,15 @@
 const allReservationsRadio = document.getElementById('allReservations');
 const myReservationsRadio = document.getElementById('myReservations');
+const recordChangesButton = document.querySelector('.filterReservations button');
 
-myReservationsRadio.addEventListener('click', function() {
-        allReservationsRadio.checked = false;
-        myReservationsRadio.checked = true;
+recordChangesButton.addEventListener('click', function(event) {
+  event.preventDefault();
+  if (allReservationsRadio.checked) {
+    allReservationsRadio.checked = true;
+    myReservationsRadio.checked = false;
+  } else if (myReservationsRadio.checked) {
+    allReservationsRadio.checked = false;
+    myReservationsRadio.checked = true;
+  }
 });
+
