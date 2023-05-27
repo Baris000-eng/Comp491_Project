3352069352c6272a9c85ea5d2707157eba01b991
vakin18 <79648295+vakin18@users.noreply.github.com>
2023-05-27 17:45:34 +0300

    Change inside classroom grid to 4x4

--- static/pagination_classroom_view.js
+++ static/pagination_classroom_view.js
@@ -1,7 +1,7 @@
 function showPage(pageNumber) {
   const images = document.querySelectorAll("#image-container img");
-  const startingIndex = (pageNumber - 1) * 9;
-  let endingIndex = startingIndex + 9;
+  const startingIndex = (pageNumber - 1) * 16;
+  let endingIndex = startingIndex + 16;
 
   if (endingIndex > images.length) {
     endingIndex = images.length;
